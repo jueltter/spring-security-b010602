0187177bf4c58eeccce08da706f49875cdaf7030
Stalin Amagua <jueltter@users.noreply.github.com>
2026-02-24 00:23:50 -0500

    - bye endpoint added

--- src/main/java/tech/samagua/spring_security_b010602/config/ProjectConfig.java
+++ src/main/java/tech/samagua/spring_security_b010602/config/ProjectConfig.java
@@ -1,28 +1,18 @@
 package tech.samagua.spring_security_b010602.config;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @Configuration
-@RequiredArgsConstructor
+@EnableAsync
 public class ProjectConfig {
 
-    private final AuthenticationProvider authenticationProvider;
-
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.httpBasic(Customizer.withDefaults());
-
-        http.authenticationProvider(authenticationProvider);
-
-        http.authorizeHttpRequests(c -> c.anyRequest().authenticated());
-
-        return http.build();
+    public InitializingBean initializingBean() {
+        return () -> SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
     }
 
 }
--- src/main/java/tech/samagua/spring_security_b010602/controllers/HelloController.java
+++ src/main/java/tech/samagua/spring_security_b010602/controllers/HelloController.java
@@ -1,10 +1,14 @@
 package tech.samagua.spring_security_b010602.controllers;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HelloController {
 
     @GetMapping("/hello")
@@ -14,6 +18,16 @@ public class HelloController {
         //var authentication = securityContext.getAuthentication();
 
         return "Hello, " +authentication.getName()+ "!";
+    }
+
+    @GetMapping("/bye")
+    @Async
+    public void bye() {
+        var securityContext = SecurityContextHolder.getContext();
+        var authentication = securityContext.getAuthentication();
+
+
+        log.info("Goodbye, {}!", authentication.getName());
     }
 
 }
