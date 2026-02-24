75174f3f7448b7dae5dba338fb58ae7afcb9fa6d
Stalin Amagua <jueltter@users.noreply.github.com>
2026-02-23 23:46:50 -0500

    - username obtained from security context

--- src/main/java/tech/samagua/spring_security_b010602/controllers/HelloController.java
+++ src/main/java/tech/samagua/spring_security_b010602/controllers/HelloController.java
@@ -1,5 +1,7 @@
 package tech.samagua.spring_security_b010602.controllers;
 
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +10,11 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello!";
+
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        var authentication = securityContext.getAuthentication();
+
+        return "Hello, " +authentication.getName()+ "!";
     }
 
 }
