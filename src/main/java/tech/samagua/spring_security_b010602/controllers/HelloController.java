de15972a0b392a9dfb7f7e3275016f2fcf26dd5d
Stalin Amagua <jueltter@users.noreply.github.com>
2026-02-24 00:46:23 -0500

    - ciao endpoint added

--- src/main/java/tech/samagua/spring_security_b010602/controllers/HelloController.java
+++ src/main/java/tech/samagua/spring_security_b010602/controllers/HelloController.java
@@ -2,10 +2,15 @@ package tech.samagua.spring_security_b010602.controllers;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.security.concurrent.DelegatingSecurityContextCallable;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @RestController
 @Slf4j
@@ -28,6 +33,21 @@ public class HelloController {
 
 
         log.info("Goodbye, {}!", authentication.getName());
+    }
+
+    @GetMapping("/ciao")
+    public String ciao() throws Exception {
+        Callable<String> task = () -> {
+            var securityContext = SecurityContextHolder.getContext();
+            var authentication = securityContext.getAuthentication();
+
+            return "Ciao, " + authentication.getName() + "!";
+        };
+
+        try(ExecutorService executorService = Executors.newCachedThreadPool();) {
+            var contextTask = new DelegatingSecurityContextCallable<>(task);
+            return executorService.submit(contextTask).get();
+        }
     }
 
 }
